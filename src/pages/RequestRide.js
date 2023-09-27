6b0db0db8fa669108ee8270113e360d82d81da12
Ethan Park <ethanpark5300@gmail.com>
2023-09-27 16:17:21 -0700

    Edited Booking page title to RequestRide, edited Map page title to TrackRide, and removed About page

---
+++ src/pages/RequestRide.js
@@ -0,0 +1,12 @@
+import '../styles/RequestRide.css';
+
+function RequestRide() 
+{
+    return (
+        <div className='RequestRide'>
+            <h1>Request Ride</h1>
+        </div>
+    );
+}
+
+export default RequestRide;
---
+++ src/pages/TrackRide.js
@@ -0,0 +1,12 @@
+import '../styles/TrackRide.css';
+
+function TrackRide() 
+{
+    return (
+        <div className='TrackRide'>
+            <h1>Track Ride</h1>
+        </div>
+    );
+}
+
+export default TrackRide;
