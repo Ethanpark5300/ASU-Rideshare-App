20b37fd3b7275c70e871238054f991a41133fba1
Ethan Park <ethanpark5300@gmail.com>
2023-09-27 16:42:45 -0700

    Added error element in index.js

--- src/index.js
+++ src/index.js
@@ -24,7 +24,7 @@ const router = createBrowserRouter
     [
         {
             element: <AppLayout />,
-            //errorElement: <ErrorPage />,
+            errorElement: <ErrorPage />,
             children: 
             [
                 {
