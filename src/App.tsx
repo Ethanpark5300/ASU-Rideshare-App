237ea8b34a020a398f9edb8e940229b2722f30ee
Alophox <bluebolt1@hotmail.com>
2023-09-30 14:20:27 -0700

    most of index.js transferred to App.tsx index.tsx now handles init now works in vs

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register"
+import RequestRide from "./pages/RequestRide";
+import TrackRide from "./pages/TrackRide";
+import RideHistory from "./pages/RideHistory";
+import Payment from "./pages/Payment";
+import Profile from "./pages/Profile";
+import ErrorPage from "./pages/ErrorPage";
 
-export default App;
+const AppLayout = () => 
+{
+    return (
+        <> 
+            <Outlet />
+        </>
+    );
+};
+
+const router = createBrowserRouter
+(
+    [
+        {
+            element: <AppLayout />,
+            errorElement: <ErrorPage />,
+            children: 
+            [
+                {
+                    path: "/",
+                    element: <Home />,
+                },
+                {
+                    path: "Login",
+                    element: <Login />,
+                },
+                {
+                    path: "Register",
+                    element: <Register />,
+                },
+                {
+                    path: "RequestRide",
+                    element: <RequestRide />,
+                },
+                {
+                    path: "TrackRide",
+                    element: <TrackRide />,
+                },
+                {
+                    path: "RideHistory",
+                    element: <RideHistory />,
+                },
+                {
+                    path: "Payment",
+                    element: <Payment />,
+                },
+                {
+                    path: "Profile",
+                    element: <Profile />,
+                },
+
+            ],
+        },
+    ]
+);
+
+export default router;
