c3a29c9f8f189169de00de66753e29e19425f82b
Colton Onishi <94552617+RegentSet40@users.noreply.github.com>
2023-10-17 11:12:26 -0700

    Add files via upload
    creates new user for login system

---
+++ src/databases/New_User.sql
@@ -0,0 +1,11 @@
+/*When a person is creating a new account, it will check if account is created and create an account 
+if there is no repeat
+<> = placeholder for user input that will be filled in */
+INSERT INTO USER_INFO (First_Name,Last_Name,Username,Password,Email)
+SELECT '<First_Name>','<Last_Name>','<Username>','<Password>','<Email>'
+WHERE NOT EXISTS (SELECT First_Name,Last_Name,Username,Password,Email FROM USER_INFO
+WHERE First_Name ='<First_Name>'
+AND Last_Name = '<Last_Name>'
+AND Username = '<Username>'
+AND Password = '<Password>'
+AND Email = '<Email>');
