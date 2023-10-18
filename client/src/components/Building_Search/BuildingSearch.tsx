11fc0c859434a63d585a2080fa2118a85258762b
Ethan Park <ethanpark5300@gmail.com>
2023-10-17 19:15:49 -0700

    Added builing search code

---
+++ client/src/components/Building_Search/BuildingSearch.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from 'react';
+import buildingsData from '../../databases/Buildings.json';
+import Select from 'react-select';
+import './BuildingSearch.css';
+
+interface Building {
+  code: string;
+  name: string;
+  address: string;
+}
+
+interface BuildingOption {
+  value: string;
+  label: string;
+  address: string;
+}
+
+function BuildingSearch() {
+  const [selectedBuilding, setSelectedBuilding] = useState<BuildingOption | null>(null);
+
+  const buildingOptions: BuildingOption[] = buildingsData.buildings.map((building: Building) => ({
+    value: building.code,
+    label: `${building.code} - ${building.name} (${building.address})`,
+    address: building.address,
+  }));
+
+  const handleBuildingChange = (selectedOption: BuildingOption | null) => {
+    setSelectedBuilding(selectedOption);
+  };
+
+  const handleClearSearch = () => {
+    setSelectedBuilding(null);
+  };
+
+  const customOption = ({ innerProps, label, isFocused }: any) => (
+    <div {...innerProps} className={isFocused ? 'option-focused' : 'option'}>
+      <div>{label}</div>
+    </div>
+  );
+
+  return (
+    <div className="BuildingSearch">
+      <h3>Building Search</h3>
+      <Select
+        classNamePrefix="search-bar"
+        options={buildingOptions}
+        value={selectedBuilding}
+        onChange={handleBuildingChange}
+        isSearchable
+        placeholder="Search for a building..."
+        components={{ Option: customOption }}
+      />
+      <button className="ClearButton" onClick={handleClearSearch}>
+        Clear
+      </button>
+      {selectedBuilding && (
+        <div className="SelectedBuilding">
+          <h3>Selected Building</h3>
+          <p>
+            Code: {selectedBuilding.value} <br />
+            Name: {selectedBuilding.label.split(' - ')[1].split(' (')[0]} <br />
+            Address: {selectedBuilding.address}
+          </p>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default BuildingSearch;
--- client/src/pages/RequestRide.tsx
+++ client/src/pages/RequestRide.tsx
@@ -1,5 +1,6 @@
 import '../styles/RequestRide.css';
 import Navbar from "../components/Navigation_Bar/Navbar";
+import BuildingSearch from '../components/Building_Search/BuildingSearch';
 
 function RequestRide() 
 {
@@ -7,6 +8,9 @@ function RequestRide()
         <div className='RequestRide'>
             <Navbar />
             <h1>Request Ride</h1>
+            <br/>
+            <h2>Building Search</h2>
+            <BuildingSearch />
         </div>
     );
 }
