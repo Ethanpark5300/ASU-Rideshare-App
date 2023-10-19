7a504f190ac5455223f29cf6e7f9cb0b3699c7e0
fdeporzi <fdeporzi@asu.edu>
2023-10-19 11:46:30 -0700

    Added html elements to home page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -7,6 +7,12 @@ function Home()
         <div className='Home'>
             <Navbar />
             <h1>Home</h1>
+
+            <p>Welcome to Sun Devil Rideshare, an app designed to help ASU students conveniently share rides. 
+                Whether you need a lift to campus or are offering an open seat in your car, Sun Devil Rideshare makes it easy to request and offer rides within the ASU community.</p>
+
+            <h2>Cancellation Policy</h2>
+            <p>Cancellation Policy</p>
         </div>
     );
 }
