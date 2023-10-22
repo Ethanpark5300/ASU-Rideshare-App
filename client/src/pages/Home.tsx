42a345cbfbc883aba375e3b95d7fd2a63db208a6
fdeporzi <fdeporzi@asu.edu>
2023-10-21 23:32:06 -0700

    Style changes to instructions

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <div className="Home">
       <Navbar />
-      <h1>ASU Rideshare</h1>
+      <h1>Better Commuting for Sun Devils</h1>
 
       <p>
         This is an app designed to help ASU students conveniently share rides.
@@ -15,20 +15,23 @@ function Home() {
       </p>
 
       <div id="instructions-container">
-        <div id="lists-grid">
-          <ol id="rider-steps">
+        <div id="rider-container">
+          <h3>Riders</h3>
+
+          <ul id="rider-steps">
             <li>Open app and enter pickup location</li>
             <li>Browse drivers and select one headed your way</li>
             <li>Wait at pickup spot for your driver</li>
-          </ol>
+          </ul>
+        </div>
 
-          <div id="divider"></div>
-
-          <ol id="driver-steps">
+        <div id="driver-container">
+          <h3>Drivers</h3>
+          <ul id="driver-steps">
             <li>Open app and enter route details</li>
             <li>Browse ride requests along your way</li>
             <li>Pick up riders at designated spots</li>
-          </ol>
+          </ul>
         </div>
       </div>
 
