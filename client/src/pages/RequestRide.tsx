fb136e043c53a44256f678f8016c3fbc883af8ce
Zealyth <nguyencj1988@gmail.com>
2023-10-22 19:28:29 -0700

    Request ride page update 1

--- client/src/pages/RequestRide.tsx
+++ client/src/pages/RequestRide.tsx
@@ -7,11 +7,17 @@ function RequestRide()
     return (
         <div className='RequestRide'>
             <Navbar />
-            <h1>Request Ride</h1>
-            <br/>
-            <div className='building-search'>
-                <BuildingSearch />
-                <p>hello there pal</p>
+            <div className="left-column">
+                <h1>Request Ride</h1>
+                <br/>
+                <div className='building-search'>
+                    <BuildingSearch />
+                    
+                </div>
+                <button>Submit Request</button>
+            </div>
+            <div className="right-collumn">
+                <img src="mapfiller.png" alt="filler map" />
             </div>
         </div>
     );
