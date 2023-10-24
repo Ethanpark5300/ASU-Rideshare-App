50a83c550f2971a48f3513c929084b53cb1f420e
fdeporzi <fdeporzi@asu.edu>
2023-10-23 17:40:59 -0700

    Added hero section to the homepage

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -5,29 +5,31 @@ function Home() {
   return (
     <div className="Home">
       <Navbar />
-      <h1>Better Commuting for Sun Devils</h1>
 
-      <p>
-        This is an app designed to help ASU students conveniently share rides.
-        Whether you need a lift to campus or are offering an open seat in your
-        car, ASU Rideshare makes it easy to request and offer rides within the
-        ASU community.
-      </p>
+      <section className="hero">
+        <h1>Better Commuting for Sun Devils</h1>
 
-      <div id="instructions-container">
-        <div id="rider-container">
+        <p>
+          Whether you need a lift to campus or are offering an open seat in your
+          car, ASU Rideshare makes it easy to request and offer rides within the
+          ASU community.
+        </p>
+      </section>
+
+      <div className="instructions-container">
+        <div className="rider-container">
           <h3>Riders</h3>
 
-          <ul id="rider-steps">
+          <ul className="rider-steps">
             <li>Open app and enter pickup location</li>
             <li>Browse drivers and select one headed your way</li>
             <li>Wait at pickup spot for your driver</li>
           </ul>
         </div>
 
-        <div id="driver-container">
+        <div className="driver-container">
           <h3>Drivers</h3>
-          <ul id="driver-steps">
+          <ul className="driver-steps">
             <li>Open app and enter route details</li>
             <li>Browse ride requests along your way</li>
             <li>Pick up riders at designated spots</li>
@@ -35,7 +37,7 @@ function Home() {
         </div>
       </div>
 
-      <p id="cancelPolicy">
+      <p className="cancelPolicy">
         Cancellation Policy: If you need to cancel your ride request, please do
         so at least 2 hours before the scheduled pickup time. Last minute
         cancellations leave drivers unable to fill the open seat. To be fair to
