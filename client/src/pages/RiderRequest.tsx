19f73e31925181acafd71bf5845de6c59a910254
Ethan Park <ethanpark5300@gmail.com>
2023-10-24 22:46:44 -0700

    Created Rider Request page

---
+++ client/src/pages/RiderRequest.tsx
@@ -0,0 +1,13 @@
+import '../styles/RiderRequest.css';
+import Navbar from "../components/Navigation_Bar/Navbar";
+
+function RiderRequest() {
+    return (
+        <div className='RiderRequest'>
+            <Navbar />
+            <h1>Rider Request</h1>
+        </div>
+    );
+}
+
+export default RiderRequest;
