2ab4258523bc58a59f23881766a923a4ac730c4d
Ethan Park <ethanpark5300@gmail.com>
2023-10-27 15:33:06 -0700

    Fixed navbar and register link in login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -4,9 +4,9 @@ import '../styles/Login.css';
 import { useAppDispatch } from '../store/hooks';
 import { setAccountStore } from '../store/features/accountSlice';
 import { TextInput } from '../components/Text_Input/TextInput';
-import Navbar from '../components/Navigation_Bars/Rider_Navbar/Navbar';
+import Navbar from '../components/Navigation_Bars/Guest_Navbar/Navbar';
 import { DatabaseAccessor } from '../databases/DatabaseAccessor';
-import { NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { Button } from '../components/Buttons/Button';
 import PageTitle from '../components/Page_Title/PageTitle';
 
@@ -47,55 +47,54 @@ function Login() {
 
 
     return (
-		<PageTitle title="Login">
-			<div className='Login'>
+		<>
+			<Navbar />
+			<PageTitle title="Login">
+				<div className='Login'>
 
-				<div>{data ? data : "Loading"}</div>
+					<div>{data ? data : "Loading"}</div>
 
-				<h1>Login</h1>
-				<h2>Email:</h2>
-				<div>
-					<TextInput
-						placeholder=""
-						regex={/^[a-zA-Z0-9_@.]+$/}
-						valueRef={emailRef}
-						enterFunction={setAccount}
+					<h1>Login</h1>
+					<h2>Email:</h2>
+					<div>
+						<TextInput
+							placeholder=""
+							regex={/^[a-zA-Z0-9_@.]+$/}
+							valueRef={emailRef}
+							enterFunction={setAccount}
+						/>
+					</div>
+					
+					<h2>Password:</h2>
+					<div>
+						<TextInput
+							placeholder=""
+							regex={undefined}
+							valueRef={passwordRef}
+							enterFunction={setAccount}
+							inputType="password"
+						/>
+					</div>
+					
+					{
+						(loginFailed) && (
+							<p className="LoginError">
+								email or password is incorrect
+							</p>
+						)
+					}
+					<Button
+						label="login"
+						onClickFn={setAccount}
 					/>
-				</div>
-				
-				<h2>Password:</h2>
-				<div>
-					<TextInput
-						placeholder=""
-						regex={undefined}
-						valueRef={passwordRef}
-						enterFunction={setAccount}
-						inputType="password"
-					/>
-				</div>
-				
-				{
-					(loginFailed) && (
-						<p className="LoginError">
-							email or password is incorrect
-						</p>
-					)
-				}
-				<Button
-					label="login"
-					onClickFn={setAccount}
-				/>
-				<h2>Don't have an account?</h2>
-				<h2>
-				<NavLink to="/register">
-					Register
-				</NavLink>
-				</h2>
+					<h2>Don't have an account?</h2>
+					<h2> <Link to="/Register"> Register </Link> </h2>
 
 
-				
-			</div>
-		</PageTitle>
+					
+				</div>
+			</PageTitle>
+		</>
     );
 }
 
