2d3524e556bf768bdc67400cf1913eb3fd07ce21
Colton Onishi <94552617+RegentSet40@users.noreply.github.com>
2023-10-31 17:04:49 -0700

    Update New_User.sql

--- client/src/databases/New_User.sql
+++ client/src/databases/New_User.sql
@@ -3,7 +3,7 @@ if there is no repeat
 <> = placeholder for user input that will be filled in */
 INSERT INTO USER_INFO (First_Name,Last_Name,Username,Password,Email)
 SELECT '<First_Name>','<Last_Name>','<Username>','<Password>','<Email>'
-WHERE NOT EXISTS (SELECT First_Name,Last_Name,Username,Password,Email FROM USER_INFO
+WHERE NOT EXISTS (SELECT /*First_Name,Last_Name,Username,Password,*/Email FROM USER_INFO
 WHERE First_Name ='<First_Name>'
 AND Last_Name = '<Last_Name>'
 AND Username = '<Username>'
