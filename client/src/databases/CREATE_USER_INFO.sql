7d444733a29b6356734257f485dc1163923baccd
Colton Onishi <94552617+RegentSet40@users.noreply.github.com>
2023-11-02 15:26:53 -0700

    Update CREATE_USER_INFO.sql

--- client/src/databases/CREATE_USER_INFO.sql
+++ client/src/databases/CREATE_USER_INFO.sql
@@ -1,13 +1,13 @@
 --Use IDENTITY(0,1) if AUTO_INCREMENT does not work
 CREATE TABLE USER_INFO (
-	User_ID int AUTO_INCREMENT PRIMARY KEY,
+	User_ID int IDENTITY(0,1) PRIMARY KEY,
     First_Name varchar(500) NOT NULL,
     Last_Name varchar(500) NOT NULL,
     Username varchar(500) NOT NULL ,
     Password varchar(500) NOT NULL,
     Email varchar(500) NOT NULL,
-    Rating_Passenger double,
-    Rating_Driver double
+    Rating_Passenger real,
+    Rating_Driver real
 );
 
 
