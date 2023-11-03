60f60d78790bbe3c2bc45fb84aaccdef214527a3
Colton Onishi <94552617+RegentSet40@users.noreply.github.com>
2023-11-03 14:31:41 -0700

    Update CREATE_DRIVER_REVIEW_PAGE.sql

--- server/Tables/CREATE_DRIVER_REVIEW_PAGE.sql
+++ server/Tables/CREATE_DRIVER_REVIEW_PAGE.sql
@@ -1,3 +1,5 @@
+/*table is for viewing all
+will create a way to pull the information for specific drivers with a WHERE statement*/
 CREATE TABLE DRIVER_REVIEW_PAGE (
 	User_ID int NOT NULL PRIMARY KEY,
 	Rider_ID int NOT NULL,
