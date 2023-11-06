0ca02da1aea01893a959628a1095b2a76b7889b1
fdeporzi <fdeporzi@asu.edu>
2023-11-05 23:00:18 -0700

    Styling on login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -1,100 +1,94 @@
-import { useEffect, useRef, useState } from 'react';
-import { Account } from '../account/Account';
-import '../styles/Login.css';
-import { useAppDispatch } from '../store/hooks';
-import { setAccountStore } from '../store/features/accountSlice';
-import { TextInput } from '../components/Text_Input/TextInput';
-import Navbar from '../components/Navigation_Bars/Guest_Navbar/Navbar';
-import { DatabaseAccessor } from '../databases/DatabaseAccessor';
-import { Link } from 'react-router-dom';
-import { Button } from '../components/Buttons/Button';
-import PageTitle from '../components/Page_Title/PageTitle';
-
+import { useEffect, useRef, useState } from "react";
+import { Account } from "../account/Account";
+import "../styles/Login.css";
+import { useAppDispatch } from "../store/hooks";
+import { setAccountStore } from "../store/features/accountSlice";
+import { TextInput } from "../components/Text_Input/TextInput";
+import Navbar from "../components/Navigation_Bars/Guest_Navbar/Navbar";
+import { DatabaseAccessor } from "../databases/DatabaseAccessor";
+import { Link } from "react-router-dom";
+import { Button } from "../components/Buttons/Button";
+import PageTitle from "../components/Page_Title/PageTitle";
 
 function Login() {
-	let databaseAccessor: DatabaseAccessor = DatabaseAccessor.getInstance();
-	const dispatch = useAppDispatch();
+  let databaseAccessor: DatabaseAccessor = DatabaseAccessor.getInstance();
+  const dispatch = useAppDispatch();
 
-	const emailRef = useRef<string>("");
-	const passwordRef = useRef<string>("");
+  const emailRef = useRef<string>("");
+  const passwordRef = useRef<string>("");
 
-	const [loginFailed, setLoginFailed] = useState<boolean>(false);
+  const [loginFailed, setLoginFailed] = useState<boolean>(false);
 
-	function setAccount() {
-
-
-		let loginAccount: Account | undefined = databaseAccessor.login(emailRef.current, passwordRef.current);
-		if (loginAccount !== undefined) {
-			//success login
-			setLoginFailed(false);
-		} else {
-			//fail login
-			setLoginFailed(true);
-		}
-		//store account in store, or store undefined if no login made
-		dispatch(setAccountStore(loginAccount));
-		
-	}
-
-	const [data, setData] = useState(null);
-
-	useEffect(() => {
-		fetch(`/registration`, {
-			method: "POST",
-			headers: { 'Content-type': "application/json" },
-			body: JSON.stringify({user: "username", pass: "password"})
-		})
-			.then((res) => res.json())
-			.then((data) => setData(data.registrationSuccess));
-	}, []);
-
-
-
-    return (
-		<PageTitle title="Login">
-			<Navbar />
-			<div className='Login'>
-
-				<div>{data ? "Registration success!" : "Loading"}</div>
-
-				<h1>Login</h1>
-				<h2>Email:</h2>
-				<div>
-					<TextInput
-						placeholder=""
-						regex={/^[a-zA-Z0-9_@.]+$/}
-						valueRef={emailRef}
-						enterFunction={setAccount}
-					/>
-				</div>
-				
-				<h2>Password:</h2>
-				<div>
-					<TextInput
-						placeholder=""
-						regex={undefined}
-						valueRef={passwordRef}
-						enterFunction={setAccount}
-						inputType="password"
-					/>
-				</div>
-				
-				{
-					(loginFailed) && (
-						<p className="LoginError">
-							email or password is incorrect
-						</p>
-					)
-				}
-				<Button
-					label="login"
-					onClickFn={setAccount}
-				/>
-				<h2>Don't have an account?</h2>
-				<h2> <Link to="/Register"> Register </Link> </h2>					
-			</div>
-		</PageTitle>
+  function setAccount() {
+    let loginAccount: Account | undefined = databaseAccessor.login(
+      emailRef.current,
+      passwordRef.current
     );
+    if (loginAccount !== undefined) {
+      //success login
+      setLoginFailed(false);
+    } else {
+      //fail login
+      setLoginFailed(true);
+    }
+    //store account in store, or store undefined if no login made
+    dispatch(setAccountStore(loginAccount));
+  }
+
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch(`/registration`, {
+      method: "POST",
+      headers: { "Content-type": "application/json" },
+      body: JSON.stringify({ user: "username", pass: "password" }),
+    })
+      .then((res) => res.json())
+      .then((data) => setData(data.registrationSuccess));
+  }, []);
+
+  return (
+    <PageTitle title="Login">
+      <Navbar />
+      <div className="Login">
+        <div>{data ? "Registration success!" : "Loading"}</div>
+
+        <h1>Log In to Rideshare</h1>
+        <div className="login-container">
+          <h2>Email:</h2>
+          <div>
+            <TextInput
+              placeholder=""
+              regex={/^[a-zA-Z0-9_@.]+$/}
+              valueRef={emailRef}
+              enterFunction={setAccount}
+            />
+          </div>
+
+          <h2>Password:</h2>
+          <div>
+            <TextInput
+              placeholder=""
+              regex={undefined}
+              valueRef={passwordRef}
+              enterFunction={setAccount}
+              inputType="password"
+            />
+          </div>
+
+          {loginFailed && (
+            <p className="LoginError">email or password is incorrect</p>
+          )}
+          <Button label="Login" onClickFn={setAccount} />
+          <h2>Don't have an account?</h2>
+          <h2>
+            {" "}
+            <Link to="/Register"> Register </Link>{" "}
+          </h2>
+        </div>
+      </div>
+    </PageTitle>
+  );
 }
 
 export default Login;
