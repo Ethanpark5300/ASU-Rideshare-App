9b2bb60e1a8fc5090781972bde3a1e2a69ee9a84
Steven Nguyen <nguyencj1988@gmail.com>
2023-11-19 23:26:33 -0700

    Ride in progress pt1

--- client/src/pages/RideinProgress.tsx
+++ client/src/pages/RideinProgress.tsx
@@ -1,18 +1,55 @@
 import '../styles/RideinProgress.css';
 import Navbar from "../components/Navigation_Bar/Navbar";
 import PageTitle from '../components/Page_Title/PageTitle';
+import Map_filler from "../images/mapfiller.png";
+import { useState, useEffect } from 'react';
+import { Driver, Ride } from './models'; /*need a way to identify which ride is happening*/
+
+interface Props {
+  driver: Driver;
+  ride: Ride;
+}
 
 function RideinProgress() 
 {
     /* TODO: Add emergency features and a report button */
+    
     return (
         <PageTitle title="Ride in Progress">
             <Navbar />
             <div className='RideinProgress'>
                 <h1>Ride in Progress</h1>
+                <div className="map-container">
+                    <img src={Map_filler} alt="filler map" className="map"/>
+                </div>
+
+                <div className="ride-info">
+                    <div className="progress">
+                    <span>{rideProgress}%</span>
+                    </div>
+
+                    <button onClick={handleReportDriver}>
+                    Report Driver
+                    </button>
+                </div>
             </div>
         </PageTitle>
     );
 }
 
-export default RideinProgress;
+export default function RideInProgress({driver, ride}: Props) {
+
+    const [rideProgress, setRideProgress] = useState(0);
+  
+    useEffect(() => {
+      const interval = setInterval(() => {
+        // update rideProgress...
+        setRideProgress(updatedProgress); 
+      }, 1000);
+      return () => clearInterval(interval);
+    }, []);
+  
+    function handleReportDriver() {
+      // logic to report driver
+    }
+}
