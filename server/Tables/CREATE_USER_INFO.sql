4f0ec35922f1cbe42ccb7b944395df9387188416
Colton Onishi <94552617+RegentSet40@users.noreply.github.com>
2024-01-29 14:08:33 -0700

    Update CREATE_USER_INFO.sql

--- server/Tables/CREATE_USER_INFO.sql
+++ server/Tables/CREATE_USER_INFO.sql
@@ -1,17 +1,17 @@
 --Use IDENTITY(0,1) if AUTO_INCREMENT does not work
 CREATE TABLE USER_INFO (
 	 Email varchar(500) NOT NULL UNIQUE PRIMARY KEY,
-	Type int,
+	Type_User int,
     First_Name varchar(500) NOT NULL,
     Last_Name varchar(500) NOT NULL,
-    Password varchar(500) NOT NULL,
+    Password_User varchar(500) NOT NULL,
     Rating_Passenger real,
     Rating_Driver real,
 	Birthday date,
 	Phone_Number varchar(500),
 	Pay_Pal varchar(500), 
 	Session_Token varchar(500),
-	Status varchar(500)
+	Status_User varchar(500)
 );
 
 
