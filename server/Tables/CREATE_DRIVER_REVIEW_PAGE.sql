7c60f8b3d3cee55441a1ac84dad27f3e91fee9fa
Colton Onishi <94552617+RegentSet40@users.noreply.github.com>
2024-01-29 14:12:47 -0700

    changed user_id to email

--- server/Tables/CREATE_DRIVER_REVIEW_PAGE.sql
+++ server/Tables/CREATE_DRIVER_REVIEW_PAGE.sql
@@ -1,7 +1,7 @@
 /*table is for viewing all
 will create a way to pull the information for specific drivers with a WHERE statement*/
 CREATE TABLE DRIVER_REVIEW_PAGE (
-	User_ID int NOT NULL PRIMARY KEY,
+	Email varchar(500) NOT NULL UNIQUE PRIMARY KEY,
 	Rider_ID int NOT NULL,
 	Rating int,
 	Review varchar(500),
