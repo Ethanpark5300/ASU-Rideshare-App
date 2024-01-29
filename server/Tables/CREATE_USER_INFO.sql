cf17b241fef968c390d6818de438bedaac2f9b59
Colton Onishi <94552617+RegentSet40@users.noreply.github.com>
2024-01-29 14:06:10 -0700

    Update CREATE_USER_INFO.sql

--- server/Tables/CREATE_USER_INFO.sql
+++ server/Tables/CREATE_USER_INFO.sql
@@ -1,17 +1,17 @@
 --Use IDENTITY(0,1) if AUTO_INCREMENT does not work
 CREATE TABLE USER_INFO (
-	User_ID int IDENTITY(0,1) PRIMARY KEY,
-	Driver_ID int , -- need to ask if i need random generated or not
-	Rider_ID int ,
+	 Email varchar(500) NOT NULL UNIQUE PRIMARY KEY,
+	Type int,
     First_Name varchar(500) NOT NULL,
     Last_Name varchar(500) NOT NULL,
     Password varchar(500) NOT NULL,
-    Email varchar(500) NOT NULL UNIQUE,
     Rating_Passenger real,
     Rating_Driver real,
 	Birthday date,
 	Phone_Number varchar(500),
-	Pay_Pal varchar(500)
+	Pay_Pal varchar(500), 
+	Session_Token varchar(500),
+	Status varchar(500)
 );
 
 
