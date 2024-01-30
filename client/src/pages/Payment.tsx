a88b38d86d2b793642d9005d5d60c1bb4a8dfdef
Ethan Park <ethanpark5300@gmail.com>
2024-01-30 01:51:51 -0700

    Fixed navbar styles to reflect ASU brand guidelines

--- client/src/pages/Payment.tsx
+++ client/src/pages/Payment.tsx
@@ -1,32 +1,12 @@
-import React from 'react';
 import '../styles/Payment.css';
 import PageTitle from '../components/PageTitle/PageTitle';
-import PayPal from '../components/PayPal/PayPal';
 
-const Payment: React.FC = () => {
-    /**
-     * Set driver email (Temporary email atm)
-     * @TODO Dynamically update each ride
-     */
-    const driverEmail = 'sb-4swkm28693439@business.example.com';
-
-    /**
-     * Ride cost amount (Temporary cost atm)
-     * @TODO Dynamically update each ride
-     */
-    const cost = 50;
-
+function Payment() {
     return (
-        <PageTitle title="Payment">
-            <main id="payment">
-                <h1>Payment</h1>
-                <br/>
-                <h3>Rider Email: {driverEmail}</h3>
-                <h3>Ride Cost: ${cost}</h3>
-                <PayPal driverEmail={driverEmail} cost={cost} />
-            </main>
+        <PageTitle title='Payment'>
+            <h1>Payment</h1>
         </PageTitle>
     );
-};
+}
 
 export default Payment;
