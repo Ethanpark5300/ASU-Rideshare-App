5e6b5a5fb31363536c92851a5375208b293f5672
Colton Onishi <94552617+RegentSet40@users.noreply.github.com>
2024-01-29 20:00:19 -0700

    Update REPORTING.sql

--- server/Tables/REPORTING.sql
+++ server/Tables/REPORTING.sql
@@ -1,6 +1,6 @@
 --empty table right now until we confirm what exactly we want from this
 CREATE TABLE REPORTING (
-	EMAIL varchar(500),
+	EMAIL varchar(500), -- person doing the reporting
 	REPORT_ID int NOT NULL PRIMARY KEY, --actual report number 
 	REPORTED_ID int, -- person getting reported user id
 	REASON varchar(500) NOT NULL ,
