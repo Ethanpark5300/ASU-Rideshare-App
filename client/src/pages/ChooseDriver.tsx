ef0bc9bba1538a59f5870c2808ab0c93ed10e0c1
fdeporzi <fdeporzi@asu.edu>
2024-02-09 17:23:31 -0700

    choose driver page

--- client/src/pages/ChooseDriver.tsx
+++ client/src/pages/ChooseDriver.tsx
@@ -3,67 +3,75 @@ import PageTitle from "../components/PageTitle/PageTitle";
 import { useState, useEffect } from "react";
 
 interface ChooseDriverProps {
-    email: string;
+  email: string;
 }
 
 const ChooseDriver: React.FC<ChooseDriverProps> = (props) => {
-    const [driversAvailableList, setDriversAvailableList] = useState<any[]>([]);
-    const [favoriteDriversAvailableList, setFavoriteDriversAvailableList] = useState<any[]>([]);
+  const [driversAvailableList, setDriversAvailableList] = useState<any[]>([]);
+  const [favoriteDriversAvailableList, setFavoriteDriversAvailableList] =
+    useState<any[]>([]);
 
-    useEffect(() => {
-        refreshDriversList();
-    }, []);
+  useEffect(() => {
+    refreshDriversList();
+  }, []);
 
-    const refreshDriversList = async () => {
-        try {
-            const response = await fetch(`/available-drivers?riderEmail=${props.email}`);
-            const data = await response.json();
-            setFavoriteDriversAvailableList(data.availableFavoriteDrivers);
-            setDriversAvailableList(data.availableDriversList);
+  const refreshDriversList = async () => {
+    try {
+      const response = await fetch(
+        `/available-drivers?riderEmail=${props.email}`
+      );
+      const data = await response.json();
+      setFavoriteDriversAvailableList(data.availableFavoriteDrivers);
+      setDriversAvailableList(data.availableDriversList);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
-        } catch (error) {
-            console.error('Error fetching data:', error);
-        }
-    };
-   
-    return (
-        <PageTitle title="Choose Driver">
-            <main id="choose-driver">
-                <h1>Choose a Driver</h1>
+  return (
+    <PageTitle title="Choose Driver">
+      <main id="choose-driver">
+        <aside className="choosedriver-panel">
+          <h1>Choose a Driver</h1>
 
-                {/* Favorites List of Available Drivers */}
-                <h2>All Available Favorite Drivers</h2>
-                {favoriteDriversAvailableList.length > 0 ? (
-                    <div>
-                        {favoriteDriversAvailableList.map((drivers) => (
-                            <div key={drivers.Email}>
-                                <p>{drivers.First_Name}</p>
-                            </div>
-                        ))}
-                    </div>
-                ) : (
-                    <div>No drivers available.</div>
-                )}
+          {/* Favorites List of Available Drivers */}
+          <h2>All Available Favorite Drivers</h2>
+          {favoriteDriversAvailableList.length > 0 ? (
+            <div>
+              {favoriteDriversAvailableList.map((drivers) => (
+                <div key={drivers.Email}>
+                  <p>{drivers.First_Name}</p>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div>No drivers available.</div>
+          )}
 
-                {/* General List of Available Drivers */}
-                <h2>All Available Drivers</h2>
-                {driversAvailableList.length > 0 ? (
-                    <div>
-                        {driversAvailableList.map((drivers) => (
-                            <div key={drivers.Email}>
-                                <p>{drivers.First_Name}</p>
-                            </div>
-                        ))}
-                    </div>
-                ) : (
-                    <div>No drivers available.</div>
-                )}
+          {/* General List of Available Drivers */}
+          <h2>All Available Drivers</h2>
+          {driversAvailableList.length > 0 ? (
+            <div>
+              {driversAvailableList.map((drivers) => (
+                <div key={drivers.Email}>
+                  <p>{drivers.First_Name}</p>
+                </div>
+              ))}
+            </div>
+          ) : (
+            <div>No drivers available.</div>
+          )}
 
-                {/* Refresh Button */}
-                <button onClick={refreshDriversList}>Refresh List</button>
-            </main>
-        </PageTitle>
-    );
-}
+          {/* Refresh Button */}
+          <button className="refresh-btn" onClick={refreshDriversList}>
+            Refresh List
+          </button>
+        </aside>
+
+        <div className="maps-container"></div>
+      </main>
+    </PageTitle>
+  );
+};
 
 export default ChooseDriver;
