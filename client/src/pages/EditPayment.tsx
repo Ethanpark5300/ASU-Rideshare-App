562c6142a43a200071e078281104e5ba86ac3d19
Steven Nguyen <nguyencj1988@gmail.com>
2024-02-27 01:08:04 -0700

    Create editpayment pages

---
+++ client/src/pages/EditPayment.tsx
@@ -0,0 +1,15 @@
+import '../styles/EditPayment.css';
+import PageTitle from '../components/PageTitle/PageTitle';
+
+function EditPayment() {
+    return (
+        <PageTitle title="Edit Payment">
+            <main id="edit-payment">
+                <h1>Edit Payment page</h1>
+            </main>
+            {/* connects to payapl would only be changing email. */}
+        </PageTitle>
+    );
+}
+
+export default EditPayment;
