7d038632fccf6009953f180d7acd3e94100bf191
RegentSet40 <conish2228@gmail.com>
2024-03-20 17:38:40 -0700

    polished up change password

--- client/src/pages/ChangePassword.tsx
+++ client/src/pages/ChangePassword.tsx
@@ -7,20 +7,27 @@ import { useAppSelector } from '../store/hooks';
 
 function ChangePassword() {
     const account = useAppSelector((state) => state.account);
-    const confirmationCode = useRef<string>(""); /**random password that is given */
+    const givenPassword = useRef<string>(""); /** current account password */
     const newPassword = useRef<string>(""); /** new password that user wants to use  */
     const newConfirmPassword = useRef<string>(""); /** confirm password to see if it matches newPassword*/
 
+    const [passwordFailed, setPasswordFailed] = useState<boolean>(false);
+    const [errorMsg, setErrorMsg] = useState<string | undefined>(undefined);
+
     const changePassword = async () => {
+        if (givenPassword.current !== newPassword.current) {
+            setPasswordFailed(true);
+            setErrorMsg("Passwords do not match!");
+            return;
+        }
         try {
             fetch(`/change-password`, {
                 method: "POST",
                 headers: { "Content-type": "application/json" },
                 body: JSON.stringify({
-                    userEmail: account?.account?.email,
-                    confirmationCode: confirmationCode,
+                    email: account?.account?.email,
+                    givenPassword: givenPassword,
                     newPassword: newPassword,
-                    newConfirmPassword: newConfirmPassword,
                 }),
             })
         }
@@ -33,11 +40,11 @@ function ChangePassword() {
         <PageTitle title="Change Password">
             <main id="Password">
                <h1>Change Password</h1>
-                <label>Confirmation Code</label>
+                <label>Current Password</label>
                 <TextInput
                     placeholder=""
                     regex={undefined}
-                    valueRef={confirmationCode}
+                    valueRef={givenPassword}
                     enterFunction={changePassword}
                 /> 
                 <label>New Password</label>
@@ -47,7 +54,7 @@ function ChangePassword() {
                     valueRef={newPassword}
                     enterFunction={changePassword}
                 /> 
-                <label>Confirm Password</label>
+                <label>Confirm New Password</label>
                 <TextInput
                     placeholder=""
                     regex={undefined}
@@ -55,6 +62,11 @@ function ChangePassword() {
                     enterFunction={changePassword}
                 /> 
                 <Button label="Change Password" onClickFn={changePassword} />
+                {errorMsg && <p className="PasswordError">{errorMsg}</p>}
+
+                {passwordFailed && (
+                    <p className="PasswordError"> Change Password failed!</p>
+                )}
             </main>
         </PageTitle>
     );
