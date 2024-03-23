683c1f11254975791df4351bed2153ce44970cf6
Ethan Park <ethanpark5300@gmail.com>
2024-03-23 15:09:33 -0700

    Added warnings column to user_info table

--- server/Tables/CREATE_USERINFO_TABLE.sql
+++ server/Tables/CREATE_USERINFO_TABLE.sql
@@ -1,6 +1,6 @@
 --Use IDENTITY(0,1) if AUTO_INCREMENT does not work
 CREATE TABLE USER_INFO (
-	 Email varchar(500) NOT NULL UNIQUE PRIMARY KEY,
+	Email varchar(500) NOT NULL UNIQUE PRIMARY KEY,
 	Type_User int,
     First_Name varchar(500) NOT NULL,
     Last_Name varchar(500) NOT NULL,
@@ -9,7 +9,6 @@ CREATE TABLE USER_INFO (
     Rating_Driver real,
 	Phone_Number varchar(500),
 	Pay_Pal varchar(500), 
-	Status_User varchar(500)
+	Status_User varchar(500),
+    Warnings int
 );
-
-
