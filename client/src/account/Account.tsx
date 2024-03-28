d30f6f05e62e94ca4c59401d02ccc5d5123e238e
Alophox <bluebolt1@hotmail.com>
2024-03-28 13:04:07 -0700

    Cleaned up Account

--- client/src/account/Account.tsx
+++ client/src/account/Account.tsx
@@ -8,10 +8,8 @@ export class Account {
 	public readonly status: string;
 
 
-	//do not store password
-	//or do, as long as we don't store the hashed password
+
 	constructor(email: string, firstName: string, lastName: string, phoneNumber: string, accountType:number, payPalEmail: string, status: string) {
-		//TODO more stuff here
 		this.email = email;
 		this.firstName = firstName;
 		this.lastName = lastName;
@@ -28,7 +26,7 @@ export class Account {
  * A driver has a value of 2, or 10
  * A driver+rider has a value of 3, or 11
  * When comparing flags, use bitwise and: &. Returns 0 for no matches, or some number corresponding to matches.
- * When adding flasfs together, use bitwise or: |. Returns combination of both sets of flags.
+ * When adding flags together, use bitwise or: |. Returns combination of both sets of flags.
  */
 export enum AccountTypeFlag {
 	None = 0, //no standard account should have none here
