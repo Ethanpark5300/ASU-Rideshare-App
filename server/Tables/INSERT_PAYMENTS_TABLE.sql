c19e022ff1ea9d43f070aedd14e8fd6a5d1b58ce
Colton Onishi <94552617+RegentSet40@users.noreply.github.com>
2024-04-01 08:47:18 -0700

    fixed payments

---
+++ server/Tables/INSERT_PAYMENTS_TABLE.sql
@@ -0,0 +1,12 @@
+INSERT INTO PAYMENTS(Rider_ID, Driver_ID, Rider_Cost, Date, Time)
+VALUES
+	('zealsmeal@asu.edu',	'accidentallydemolish@asu.edu',	5,	'2024-1-23',	'11:30 AM'),
+	('fictionalizesplatter@asu.edu',	'propagandizefamously@asu.edu',	5,	'2024-3-12',	'9:45 AM'),
+	('collectivizeprovided@asu.edu',	'hmincidentallyduring@asu.edu',	5,	'2024-3-12',	'6:15 PM'),
+	('pathogenesisskeleton@asu.edu',	'malnutritionstraddle@asu.edu',	5,	'2024-3-4',	'7:45 AM'),
+	('aboutthanaccessorize@asu.edu',	'conscientisepaginate@asu.edu',	5,	'2024-3-9',	'8:20 AM'),
+	('howconservationnever@asu.edu',	'conventionaltoo@asu.edu',	5,	'2024-2-21',	'3:15 PM'),
+	('underachieveack@asu.edu',	'aboveevangelize@asu.edu',	5,	'2024-1-17',	'5:30 PM'),
+	('consequentlywhy@asu.edu',	'hmphupliftingly@asu.edu',	5,	'2024-3-25',	'7:16 AM'),
+	('overexertionaha@asu.edu',	'boohootreasured@asu.edu',	5,	'2024-2-29',	'6:30 PM'),
+	('underneaththroughout@asu.edu',	'mechanicallygah@asu.edu',	5,	'2024-2-27',	'9:15 PM'),
