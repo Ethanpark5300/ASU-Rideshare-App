5a7387e0989e51a33a3cb9fe561a39f0327f9826
Colton Onishi <94552617+RegentSet40@users.noreply.github.com>
2024-04-01 08:24:52 -0700

    fixed ratings

---
+++ server/Tables/INSERT_RATINGS_TABLE.sql
@@ -0,0 +1,12 @@
+INSERT INTO RATINGS(Rater_ID,Ratee_ID,Star_Rating,Comments,Date,Time)
+VALUES
+	('zealsmeal@asu.edu','howconservationnever@asu.edu',1,'Bad','2024-1-18','9:30 PM'),
+	('zealsmeal@asu.edu','conventionaltoo@asu.edu',5,'Great','2024-3-16','5:45 PM'),
+	('accidentallydemolish@asu.edu','underachieveack@asu.edu'4,'Good','2024-2-21','3:16 PM'),
+	('propagandizefamously@asu.edu',	'aboveevangelize@asu.edu',	5,	'Great',	'2024-3-25',	'9:30 AM'),
+	('collectivizeprovided@asu.edu',	'consequentlywhy@asu.edu',	3,	'Ok',	'2024-3-4',	'12:18 PM'),
+	('collectivizeprovided@asu.edu',	'hmphupliftingly@asu.edu',	2,	'Meh',	'2024-1-26',	'6:30 PM'),
+	('hmincidentallyduring@asu.edu',	'overexertionaha@asu.edu',	2,	'Meh',	'2024-1-30',	'8:19 AM'),
+	('pathogenesisskeleton@asu.edu',	'boohootreasured@asu.edu',	1,	'Bad',	'2024-3-16',	'9:18 AM'),
+	('malnutritionstraddle@asu.edu',	'underneaththroughout@asu.edu',	1,	'Bad',	'2024-3-5',	'10:50 AM'),
+	('malnutritionstraddle@asu.edu',	'mechanicallygah@asu.edu',	5,	'Great',	'2024-2-15',	'9:30 PM')
