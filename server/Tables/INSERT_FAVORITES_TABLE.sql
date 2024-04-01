8635f2e03be9096eba2b2055872aa8fa3435b09e
Colton Onishi <94552617+RegentSet40@users.noreply.github.com>
2024-04-01 09:26:19 -0700

    fixed favorites table

---
+++ server/Tables/INSERT_FAVORITES_TABLE.sql
@@ -0,0 +1,12 @@
+INSERT INTO FAVORITES(Favorite_ID, Rider_ID, Driver_ID, Date, Time, Status)
+VALUES
+	('zealsmeal@asu.edu',	'accidentallydemolish@asu.edu',	'2024-1-11',	'3:30 PM', 'ONLINE'),
+	('fictionalizesplatter@asu.edu',	'propagandizefamously@asu.edu',	'2024-2-19',	'2:12 PM',	'ONLINE'),
+	('collectivizeprovided@asu.edu',	'hmincidentallyduring@asu.edu',	'2024-2-5',	'3:12 PM',	'ONLINE'),
+	('pathogenesisskeleton@asu.edu',	'malnutritionstraddle@asu.edu',	'2024-2-14',	'9:30 AM',	'ONLINE'),
+	('aboutthanaccessorize@asu.edu',	'conscientisepaginate@asu.edu',	'2024-1-9',	'7:12 AM',	'ONLINE'),
+	('howconservationnever@asu.edu',	'conventionaltoo@asu.edu',	'2024-2-19',	'8:20 AM',	'ONLINE'),
+	('underachieveack@asu.edu',	'aboveevangelize@asu.edu',	'2024-3-18',	'9:15 PM',	'ONLINE'),
+	('consequentlywhy@asu.edu',	'hmphupliftingly@asu.edu',	'2024-3-14',	'11:16 AM',	'ONLINE'),
+	('overexertionaha@asu.edu',	'boohootreasured@asu.edu',	'2024-2-9',	'12:16 PM',	'ONLINE'),
+	('zealsmeal@asu.edu',	'mechanicallygah@asu.edu',	'2024-3-24',	'3:19 PM',	'ONLINE'),
