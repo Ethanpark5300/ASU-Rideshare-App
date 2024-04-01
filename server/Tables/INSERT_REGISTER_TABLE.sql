3ca667af896cc78649ffdcd0961dd6e84367eba9
Colton Onishi <94552617+RegentSet40@users.noreply.github.com>
2024-04-01 11:33:49 -0700

    added register dummy data

---
+++ server/Tables/INSERT_REGISTER_TABLE.sql
@@ -0,0 +1,12 @@
+INSERT INTO REGISTER(Email, First_Name, Last_Name, Password_User, Register_ID)
+VALUES
+	('zealsmeal@asu.edu','zeal','smeal','$2a$10$U/4oET/nT6fzpOnB72oNCOXyEEQt2kwbK7/Drek3/4EpPIkSx6vHK',''),
+	('accidentallydemolish@asu.ed','accidentally','demolish','$2a$10$m1TLFSUBalwb7qXN4CJ7vee5a.rKvnXYjk6mJho1ZhpoUSI/QvNQS','11111'),
+	('fictionalizesplatter@asu.edu','fictionalize','splatter','$2a$10$qCH0yEsvmMBYSddGxPh9k.Nr3L6MsRxxE2wXwSeuOrt0LxwCkcYl.','11112'),
+	('propagandizefamously@asu.edu','propagandize','famously','$2a$10$nWBf4CUB01c.6OZEV6hs4.wPrXwXDnzbNMttTbPU98lfgywTr/xby','11113'),
+	('collectivizeprovided@asu.edu','collectivize','provided','$2a$10$knxMPd0T4LRQPvkh5SVEHO1N2oECo2V8.a1hYWKLYx9MBo2G.Iyb.','11114'),
+	('hmincidentallyduring@asu.edu','hmincidentally','during','$2a$10$kKrQ9l2xInTYJW/IreOhXedu/Yzqo79PO1jMwTo4/Z08m0rFv0RVu','11115'),
+	('pathogenesisskeleton@asu.edu','pathogenesis','skeleton','$2a$10$7jf9pP8Mse0V/D3R1Sw3E.ZvGJSVTaHC0Q8LVeXUOi69PRqMVdKOe','11116'),
+	('malnutritionstraddle@asu.edu','malnutrition','straddle','$2a$10$KkIWQMTVjYobQBNuaNOBFOhJhtfDhlw0Nsi1Wi8qboP.moe0uNORa','11117'),
+	('aboutthanaccessorize@asu.edu','aboutthan','accessorize','$2a$10$M6H5pfFXtm35GPTsIPFCzuqkL7Cif8yAduVU.6j8FnNgmuOLZfnQm','11118'),
+	('conscientisepaginate@asu.edu','conscientise','paginate','$2a$10$1TwmfYTeZB856leYPTBEU.OTjOQ5u2ZhH3/Zh8dDCSVCZvt3k3bdq','11119');
