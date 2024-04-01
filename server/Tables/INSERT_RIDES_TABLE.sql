dfd01ec2e9bb2aa7f5f5ae6838872839e7f3364a
Colton Onishi <94552617+RegentSet40@users.noreply.github.com>
2024-04-01 11:18:23 -0700

    added rides dummy data

---
+++ server/Tables/INSERT_RIDES_TABLE.sql
@@ -0,0 +1,12 @@
+INSERT INTO RIDES(Rider_ID, Driver_ID, Pickup_Location, Pickup_time, Dropoff_Location, Dropoff_Time, Ride_Cost, Given_Rider_Rating,Given_Driver_Rating,Ride_Date,Status)
+VALUES	
+	('zealsmeal@asu.edu','accidentallydemolish@asu.edu','insert location','insert time','insert drop','insert droptime',5,2,3,'2024-2-16','COMPLETE'),
+	('fictionalizesplatter@asu.edu','propagandizefamously@asu.edu','insert location','insert time','insert drop','insert droptime',5,1,5,'2024-3-16','COMPLETE'),
+	('collectivizeprovided@asu.edu','hmincidentallyduring@asu.edu','insert location','insert time','insert drop','insert droptime',5,2,2,'2024-1-16','COMPLETE'),
+	('pathogenesisskeleton@asu.edu','malnutritionstraddle@asu.edu','insert location','insert time','insert drop','insert droptime',5,4,5,'2024-2-12','COMPLETE'),
+	('aboutthanaccessorize@asu.edu','conscientisepaginate@asu.edu','insert location','insert time','insert drop','insert droptime',5,3,4,'2024-2-07','COMPLETE'),
+	('howconservationnever@asu.edu','conventionaltoo@asu.edu','insert location','insert time','insert drop','insert droptime',5,1,1,'2024-1-20','COMPLETE'),
+	('underachieveack@asu.edu','aboveevangelize@asu.edu','insert location','insert time','insert drop','insert droptime',5,3,3,'2024-1-29','COMPLETE'),
+	('consequentlywhy@asu.edu','hmphupliftingly@asu.edu','insert location','insert time','insert drop','insert droptime',5,3,3,'2024-3-30','COMPLETE'),
+	('overexertionaha@asu.edu','boohootreasured@asu.edu','insert location','insert time','insert drop','insert droptime',5,4,3,'2024-2-01','COMPLETE'),
+	('underneaththroughout@asu.edu','mechanicallygah@asu.edu','insert location','insert time','insert drop','insert droptime',5,5,5,'2024-1-19','COMPLETE');
