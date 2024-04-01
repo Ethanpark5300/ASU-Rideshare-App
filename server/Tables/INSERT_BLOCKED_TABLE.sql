46560e05e252d867dc713fef3587013d0f0b1ea1
Colton Onishi <94552617+RegentSet40@users.noreply.github.com>
2024-04-01 09:44:56 -0700

    fixed blocked table

---
+++ server/Tables/INSERT_BLOCKED_TABLE.sql
@@ -0,0 +1,12 @@
+INSERT INTO BLOCKED(Blocker_ID, Blockee_ID, Date, Time)
+VALUES
+	('zealsmeal@asu.edu',	'collectivizeprovided@asu.edu',	'2024-1-13',	'9:30 AM'),
+	('underachieveack@asu.edu',	'collectivizeprovided@asu.edu',	'2024-3-14',	'8:37 AM'),
+	('boohootreasured@asu.edu',	'collectivizeprovided@asu.edu',	'2024-3-8',	'7:59 PM'),
+	('conscientisepaginate@asu.edu',	'aboutthanaccessorize@asu.edu',	'2024-3-11',	'12:30 PM'),
+	('consequentlywhy@asu.edu','pathogenesisskeleton@asu.edu',	'2024-3-29',	'1:45 PM'),
+	('propagandizefamously@asu.edu',	'boohootreasured@asu.edu',	'2024-2-7',	'10:30 AM'),
+	('accidentallydemolish@asu.edu',	'collectivizeprovided@asu.edu',	'2024-2-17',	'9:35 PM'),
+	('mechanicallygah@asu.edu',	'conscientisepaginate@asu.edu',	'2024-2-27',	'11:47 AM'),
+	('meaningfullyoff@asu.edu',	'underneaththroughout@asu.edu',	'2024-2-12',	'3:40 PM'),
+	('meaningfullyoff@asu.edu',	'mechanicallygah@asu.edu',	'2024-3-29',	'5:00 PM'),
