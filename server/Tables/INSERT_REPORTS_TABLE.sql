46568fa1df4919851da7eb070fd2b23ddb70528f
Colton Onishi <94552617+RegentSet40@users.noreply.github.com>
2024-04-01 10:22:26 -0700

    Add files via upload

---
+++ server/Tables/INSERT_REPORTS_TABLE.sql
@@ -0,0 +1,13 @@
+/*reusing same user info from blocked table*/
+INSERT INTO REPORTS(Reporter_ID, Reportee_ID, Reason, Comments, Date, Time)
+VALUES	
+	('zealsmeal@asu.edu',	'collectivizeprovided@asu.edu', 'insert reason'	, 'insert comment', '2024-1-13',	'9:30 AM'),
+	('underachieveack@asu.edu',	'collectivizeprovided@asu.edu', 'insert reason'	, 'insert comment',	'2024-3-14',	'8:37 AM'),
+	('boohootreasured@asu.edu',	'collectivizeprovided@asu.edu',	'insert reason'	, 'insert comment', '2024-3-8',	'7:59 PM'),
+	('conscientisepaginate@asu.edu',	'aboutthanaccessorize@asu.edu', 'insert reason'	, 'insert comment',	'2024-3-11',	'12:30 PM'),
+	('consequentlywhy@asu.edu',	'pathogenesisskeleton@asu.edu', 'insert reason'	, 'insert comment',	'2024-3-29',	'1:45 PM'),
+	('propagandizefamously@asu.edu',	'boohootreasured@asu.edu', 'insert reason'	, 'insert comment',	'2024-2-7',	'10:30 AM'),
+	('accidentallydemolish@asu.edu',	'collectivizeprovided@asu.edu',  'insert reason', 'insert comment',	'2024-2-17',	'9:35 PM'),
+	('mechanicallygah@asu.edu',	'conscientisepaginate@asu.edu', 'insert reason', 'insert comment',	'2024-2-27',	'11:47 AM'),
+	('meaningfullyoff@asu.edu',	'underneaththroughout@asu.edu', 'insert reason', 'insert comment',	'2024-2-12',	'3:40 PM'),
+	('meaningfullyoff@asu.edu',	'mechanicallygah@asu.edu',  'insert reason', 'insert comment',	'2024-3-29',	'5:00 PM');
