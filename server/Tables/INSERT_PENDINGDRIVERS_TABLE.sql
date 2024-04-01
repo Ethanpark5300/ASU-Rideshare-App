af623d89befe7ebe171c4aa052a3e626a1a6c775
Colton Onishi <94552617+RegentSet40@users.noreply.github.com>
2024-04-01 10:53:19 -0700

    pending drivers dummy data added

---
+++ server/Tables/INSERT_PENDINGDRIVERS_TABLE.sql
@@ -0,0 +1,12 @@
+INSERT INTO PENDINGDRIVERS(Rider_ID, Driver_ID)
+VALUES
+	('zealsmeal@asu.edu','accidentallydemolish@asu.edu'),
+	('fictionalizesplatter@asu.edu','propagandizefamously@asu.edu'),
+	('collectivizeprovided@asu.edu','hmincidentallyduring@asu.edu'),
+	('pathogenesisskeleton@asu.edu','malnutritionstraddle@asu.edu'),
+	('aboutthanaccessorize@asu.edu','conscientisepaginate@asu.edu'),
+	('howconservationnever@asu.edu','conventionaltoo@asu.edu'),
+	('underachieveack@asu.edu','aboveevangelize@asu.edu'),
+	('consequentlywhy@asu.edu','hmphupliftingly@asu.edu'),
+	('overexertionaha@asu.edu','boohootreasured@asu.edu'),
+	('underneaththroughout@asu.edu','mechanicallygah@asu.edu');
