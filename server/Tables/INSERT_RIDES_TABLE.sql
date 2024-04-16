79fa3227797810a7f5178ea12a9b5e320ac3fe20
Colton Onishi <94552617+RegentSet40@users.noreply.github.com>
2024-04-16 08:32:31 -0700

    added actual addresses for dummy data

--- server/Tables/INSERT_RIDES_TABLE.sql
+++ server/Tables/INSERT_RIDES_TABLE.sql
@@ -1,12 +1,12 @@
 INSERT INTO RIDES(Rider_ID, Driver_ID, Pickup_Location, Pickup_time, Dropoff_Location, Dropoff_Time, Ride_Cost, Given_Rider_Rating,Given_Driver_Rating,Ride_Date,Status)
 VALUES	
-	('zealsmeal@asu.edu','accidentallydemolish@asu.edu','insert location','insert time','insert drop','insert droptime',5,2,3,'2024-2-16','COMPLETED'),
-	('fictionalizesplatter@asu.edu','propagandizefamously@asu.edu','insert location','insert time','insert drop','insert droptime',5,1,5,'2024-3-16','COMPLETED'),
-	('collectivizeprovided@asu.edu','hmincidentallyduring@asu.edu','insert location','insert time','insert drop','insert droptime',5,2,2,'2024-1-16','COMPLETED'),
-	('pathogenesisskeleton@asu.edu','malnutritionstraddle@asu.edu','insert location','insert time','insert drop','insert droptime',5,4,5,'2024-2-12','COMPLETED'),
-	('aboutthanaccessorize@asu.edu','conscientisepaginate@asu.edu','insert location','insert time','insert drop','insert droptime',5,3,4,'2024-2-07','COMPLETED'),
-	('howconservationnever@asu.edu','conventionaltoo@asu.edu','insert location','insert time','insert drop','insert droptime',5,1,1,'2024-1-20','COMPLETED'),
-	('underachieveack@asu.edu','aboveevangelize@asu.edu','insert location','insert time','insert drop','insert droptime',5,3,3,'2024-1-29','COMPLETED'),
-	('consequentlywhy@asu.edu','hmphupliftingly@asu.edu','insert location','insert time','insert drop','insert droptime',5,3,3,'2024-3-30','COMPLETED'),
-	('overexertionaha@asu.edu','boohootreasured@asu.edu','insert location','insert time','insert drop','insert droptime',5,4,3,'2024-2-01','COMPLETED'),
-	('underneaththroughout@asu.edu','mechanicallygah@asu.edu','insert location','insert time','insert drop','insert droptime',5,5,5,'2024-1-19','COMPLETED');
+	('zealsmeal@asu.edu','accidentallydemolish@asu.edu','1200-1230 S Chief Jones Way, Tempe, AZ 85281','23 minutes','ARIZONA STATE UNIVERSITY','7 minutes',5,2,3,'2024-2-16','COMPLETED'),
+	('fictionalizesplatter@asu.edu','propagandizefamously@asu.edu','1876 E Hayden Ln, Tempe, AZ 85281','16 minutes','ARIZONA STATE UNIVERSITY','7 minutes',5,1,5,'2024-3-16','COMPLETED'),
+	('collectivizeprovided@asu.edu','hmincidentallyduring@asu.edu','2222 E Main St, Mesa, AZ 85213','12 minutes','ARIZONA STATE UNIVERSITY','26 minutes',5,2,2,'2024-1-16','COMPLETED'),
+	('pathogenesisskeleton@asu.edu','malnutritionstraddle@asu.edu','2457 E Melrose St, Mesa, AZ 85213','20 minutes','ARIZONA STATE UNIVERSITY','25 minutes',5,4,5,'2024-2-12','COMPLETED'),
+	('aboutthanaccessorize@asu.edu','conscientisepaginate@asu.edu','3425 E Thomas Rd #1086, Mesa, AZ 85213','10 minutes','ARIZONA STATE UNIVERSITY','25 minutes',5,3,4,'2024-2-07','COMPLETED'),
+	('howconservationnever@asu.edu','conventionaltoo@asu.edu','6532 E Odessa St, Mesa, AZ 85215','3 minutes','ARIZONA STATE UNIVERSITY','29 minutes',5,1,1,'2024-1-20','COMPLETED'),
+	('underachieveack@asu.edu','aboveevangelize@asu.edu','3328 E San Remo Ave, Gilbert, AZ 85234','4 minutes','ARIZONA STATE UNIVERSITY','29 minutes',5,3,3,'2024-1-29','COMPLETED'),
+	('consequentlywhy@asu.edu','hmphupliftingly@asu.edu','1764 E Erie St, Gilbert, AZ 85295','6 minutes','ARIZONA STATE UNIVERSITY','33 minutes',5,3,3,'2024-3-30','COMPLETED'),
+	('overexertionaha@asu.edu','boohootreasured@asu.edu','1454 E Warbler Rd, Gilbert, AZ 85297','11 minutes','ARIZONA STATE UNIVERSITY','35 minutes',5,4,3,'2024-2-01','COMPLETED'),
+	('underneaththroughout@asu.edu','mechanicallygah@asu.edu','2492 E Plum St, Gilbert, AZ 85298','15 minutes','ARIZONA STATE UNIVERSITY','40 minutes',5,5,5,'2024-1-19','COMPLETED');
